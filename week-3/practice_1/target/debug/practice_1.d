0216376982553787f64cb91d202364f598175a52
Ayomide Fakayode <ironspiderforlife@gmail.com>
2024-10-28 14:00:32 +0100

    (My Third Commit)

---
+++ week-3/practice_1/target/debug/practice_1.d
@@ -0,0 +1,1 @@
+C:\Users\Ayomide\Documents\a.fakayodeCSC101\week-3\practice_1\target\debug\practice_1.exe: C:\Users\Ayomide\Documents\a.fakayodeCSC101\week-3\practice_1\src\main.rs
---
+++ week-3/practice_7/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+  let floatwithseparator = 11_500.555_001;
+  println!("float value {}",floatwithseparator);
+
+  let intwithseparator = 50_000;
+  println!("int value {}",intwithseparator);
+}
